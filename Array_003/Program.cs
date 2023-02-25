748f8b555fc3649ba89c08a75db9d6f501803723
Olgunya78 <karaulshchikova@mail.ru>
2023-02-25 16:47:19 +0500

    add Array_003_Project_002

---
+++ Array_003/Program.cs
@@ -0,0 +1,17 @@
+﻿// У нас есть массив array с n элементов. Нам требуется найти элемент, совпадающий с некоторым значением, который может определять пользователь 
+// если array [index] = find - алгоритм завершил свою работу
+
+int[] array = {1, 125, 37, 49, 55, 64, 78, 64, 82};
+int n = array.Length;
+int find = 64;
+int index = 0;
+while (index < n)
+{
+if (array[index] == find)
+{
+  Console.WriteLine(index);
+  break;
+}
+  // index = index + 1;
+  index++;
+}
