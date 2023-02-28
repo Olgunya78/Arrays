12007fdb8e036b609cb59038c0c7bce24979b1a4
Olgunya78 <karaulshchikova@mail.ru>
2023-02-28 15:35:48 +0500

    add Seminar_002_1_Practice_001

---
+++ Seminar_002_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+
+Console.Clear ()
+int x = new Random().Next(10, 100);
+Console.WriteLine(x);
+int nam1 = x % 10;
+int nam2 = (x - nam1) /10;
+if (nam2 == nam1) {Console.WriteLine($"{nam2} равно {nam1}");}
+else if (nam1 > nam2) {Console.WriteLine($"{nam1} больше");}
+else if (nam2 > nam1) {Console.WriteLine($"{nam2} больше");}
