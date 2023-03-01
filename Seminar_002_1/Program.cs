57a61d448fa58a8794d3d0e71fc9b76f852340de
Olgunya78 <karaulshchikova@mail.ru>
2023-03-01 18:37:20 +0500

    add Seminar_002_1_var2

--- Seminar_002_1/Program.cs
+++ Seminar_002_1/Program.cs
@@ -3,10 +3,22 @@
 // 12 -> 2
 // 85 -> 8
 
+// вариант 1
+
+// int x = new Random().Next(10, 100);
+// Console.WriteLine(x);
+// int nam1 = x % 10;
+// int nam2 = (x - nam1) /10;
+// if (nam2 == nam1) {Console.WriteLine($"{nam2} равно {nam1}");}
+// else if (nam1 > nam2) {Console.WriteLine($"{nam1} больше");}
+// else if (nam2 > nam1) {Console.WriteLine($"{nam2} больше");}
+
+
+// вариант 2
 int x = new Random().Next(10, 100);
 Console.WriteLine(x);
-int nam1 = x % 10;
-int nam2 = (x - nam1) /10;
+int nam1 = x / 10;
+int nam2 = x % 10;
 if (nam2 == nam1) {Console.WriteLine($"{nam2} равно {nam1}");}
 else if (nam1 > nam2) {Console.WriteLine($"{nam1} больше");}
 else if (nam2 > nam1) {Console.WriteLine($"{nam2} больше");}
