20217690eb373f98d47f1cfb6f995155eb37d068
Olgunya78 <karaulshchikova@mail.ru>
2023-03-03 16:17:00 +0500

    add Seminar_002_1_var2

--- Seminar_002_1/Program.cs
+++ Seminar_002_1/Program.cs
@@ -3,22 +3,40 @@
 // 12 -> 2
 // 85 -> 8
 
-// вариант 1
+void zadacha91()
+{
+int x = new Random().Next(10, 100);
+Console.WriteLine(x);
+int nam1 = x % 10;
+int nam2 = (x - nam1) /10;
+if (nam1 == nam2) {Console.WriteLine($"{nam1} равно {nam2}");}
+else if (nam1 > nam2) {Console.WriteLine($"{nam1} больше, {nam2} меньше");}
+else {Console.WriteLine($"{nam2} больше, {nam1} меньше");}
+}
 
-// int x = new Random().Next(10, 100);
-// Console.WriteLine(x);
-// int nam1 = x % 10;
-// int nam2 = (x - nam1) /10;
-// if (nam2 == nam1) {Console.WriteLine($"{nam2} равно {nam1}");}
-// else if (nam1 > nam2) {Console.WriteLine($"{nam1} больше");}
-// else if (nam2 > nam1) {Console.WriteLine($"{nam2} больше");}
-
-
-// вариант 2
+void zadacha92()
+{
 int x = new Random().Next(10, 100);
 Console.WriteLine(x);
 int nam1 = x / 10;
 int nam2 = x % 10;
-if (nam2 == nam1) {Console.WriteLine($"{nam2} равно {nam1}");}
-else if (nam1 > nam2) {Console.WriteLine($"{nam1} больше");}
-else if (nam2 > nam1) {Console.WriteLine($"{nam2} больше");}
+if (nam1 == nam2) {Console.WriteLine($"{nam1} равно {nam2}");}
+else if (nam1 > nam2) {Console.WriteLine($"{nam1} больше, {nam2} меньше");}
+else {Console.WriteLine($"{nam2} больше, {nam1} меньше");}
+}
+
+void zadacha9()
+{
+Random random = new Random();
+int namber = random.Next(10, 100);
+Console.WriteLine(namber);
+int tens = namber / 10;
+int ones = namber % 10;
+if (tens == ones) {Console.WriteLine($"{tens} равно {ones}");}
+else if (tens > ones) {Console.WriteLine($"{tens} больше, {ones} меньше");}
+else {Console.WriteLine($"{ones} больше, {tens} меньше");}
+}
+
+// zadacha91();
+// zadacha92();
+zadacha9();
