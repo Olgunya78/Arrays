257ea25a1f06c21f29dae1cd29994b4c8486c70e
Olgunya78 <karaulshchikova@mail.ru>
2023-03-03 18:44:38 +0500

    add Seminar_002_2_var_2

--- Seminar_002_2/Program.cs
+++ Seminar_002_2/Program.cs
@@ -3,18 +3,36 @@
 // 782 -> 72
 // 918 -> 98
 
-// вариант 1
-// int x = new Random().Next(100, 1000);
-// Console.WriteLine(x);
-// int nam1 = x % 10;
-// int nam2 = x /100;
-// Console.Write(nam2);
-// Console.WriteLine (nam1);
+void zadacha1()
+{// вариант 1
+int x = new Random().Next(100, 1000);
+Console.WriteLine(x);
+int nam1 = x / 100;
+int nam2 = x % 10;
+Console.Write(nam1);
+Console.WriteLine (nam2);
+}
 
-// вариант 2
+void zadacha2()
+{// вариант 2
 int x = new Random().Next(100, 1000);
 Console.WriteLine(x);
 int nam1 = x / 100;
 int nam2 = x / 10;
 int nam3 = x % 10;
 Console.WriteLine($"{nam1}{nam3}");
+}
+
+void zadacha3()
+{// вариант 3
+int x = new Random().Next(100, 1000);
+int nam1 = x / 100;
+int nam2 = x % 10;
+string c = Convert.ToString (nam1) + Convert.ToString (nam2);
+Console.WriteLine(x);
+Console.WriteLine (c);
+}
+
+zadacha1();
+// zadacha2();
+// zadacha3();
