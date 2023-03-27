89e444baca73cb248c7b2899cac2de159dd93d78
Olgunya78 <karaulshchikova@mail.ru>
2023-03-27 15:37:48 +0500

    add Seminar_2_PZ_003_15

--- Seminar_2_PZ_003/Program.cs
+++ Seminar_2_PZ_003/Program.cs
@@ -3,7 +3,7 @@
 // 7 -> да
 // 1 -> нет
 
-void zadacha1()
+void variant1()
 {
 Console.Write("Введите день недели от 1 до 7: ");
 int day = Convert.ToInt32(Console.ReadLine());
@@ -14,4 +14,30 @@ else {Console.WriteLine($"рабочиий день");}
 }
 else Console.WriteLine($"нет такого дня недели на свете");
 }
-zadacha1();
+void variant2()
+{
+int day = 1;
+string[] week = {"пн", "вт", "ср", "чт", "пт", "сб", "вс"};
+if (day > 0 && day < 8)
+{Console.WriteLine(week[day-1]);
+if (day == 6 || day == 7) {Console.WriteLine($"выходной день");}
+else {Console.WriteLine($"рабочиий день");}
+}
+else Console.WriteLine($"нет такого дня недели на свете");
+}
+void variant3()
+{
+Console.Write("Введите день недели от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+string[] week = {"пн", "вт", "ср", "чт", "пт", "сб", "вс"};
+if (day > 0 && day < 8)
+{Console.WriteLine(week[day-1]);
+if (day == 6 || day == 7) {Console.WriteLine($"выходной день");}
+else {Console.WriteLine($"рабочиий день");}
+}
+else Console.WriteLine($"нет такого дня недели на свете");
+}
+
+// variant1();
+variant2();
+// variant3();
